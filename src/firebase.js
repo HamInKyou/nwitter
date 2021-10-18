cb2b4dd099c4753f5ad69bb49b560637615450ae
HamInGyu <hfire1207@gmail.com>
2021-10-18 22:31:23 +0900

    [수정] 파이어베이스 import 수정

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,4 @@
-import * as firebase from "firebase/app";
+import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
@@ -9,4 +9,4 @@ const firebaseConfig = {
   appId: process.env.REACT_APP_APP_ID,
 };
 
-export default firebase.initializeApp(firebaseConfig);
+export default initializeApp(firebaseConfig);
