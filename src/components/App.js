c1b6f00a48f7d694a5999c9979dc7ab27b1fc267
HamInGyu <hfire1207@gmail.com>
2021-10-19 18:11:16 +0900

    [추가] 파이어베이스 인증 추가

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import Router from "components/Router";
+import { authService } from "firebaseInstance";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
   return (
     <>
       <Router isLoggedIn={isLoggedIn} />
