1b1dd28edae08140035ec874432f3e803cf303a9
HamInGyu <hfire1207@gmail.com>
2021-10-21 12:54:10 +0900

    [추가] 로그아웃 / 리다이렉트 구현

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,5 +1,18 @@
+import { authService } from "firebaseInstance";
+import { useHistory } from "react-router-dom";
+
 function Profile() {
-  return <span>Profile</span>;
+  const history = useHistory();
+  const onLogOutClick = () => {
+    authService.signOut();
+    history.push("/");
+  };
+  return (
+    <>
+      <span>Profile</span>
+      <button onClick={onLogOutClick}>Log Out</button>
+    </>
+  );
 }
 
 export default Profile;
